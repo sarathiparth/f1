4200dd02f80237957dd33a46e3d791b0a10e4f9a
sarathiparth <sarathigiriparth@gmail.com>
2026-02-11 14:16:00 +0530

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <SFML/Graphics.hpp>
-#include "physicsworld.h"
-#include "f1car.h"
 
 int main()
 {
     sf::RenderWindow window(
         sf::VideoMode({800, 600}),
-        "F1 Physics Engine"
+        "Test Window"
     );
 
     while (window.isOpen())
